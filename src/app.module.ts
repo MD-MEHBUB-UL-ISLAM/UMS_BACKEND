aa7b1e0e3595c7153714c70aa16f06ec02564143
MD MEHBUB UL ISLAM <114100350+MD-MEHBUB-UL-ISLAM@users.noreply.github.com>
2023-05-12 23:47:21 +0600

    Code

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentModule } from './Module/app.StudentModule';
+import { StudentRegModule } from './Module/registermodule.module';
+import { CourseModule } from './Module/course.module';
+import { BookModule } from './Module/app.LibrarianModule';
+import { NoteModule } from './Module/app.NotesModule';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+@Module({
+  imports: [StudentModule,StudentRegModule, CourseModule,BookModule,NoteModule, TypeOrmModule.forRoot(
+   { type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'root',
+    database: 'ums',
+    autoLoadEntities: true,
+    synchronize: true,
+  }
+  ), ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', '../public'), // added ../ to get one folder back
+    serveRoot: '/public/' //last slash was important
+  }),],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
